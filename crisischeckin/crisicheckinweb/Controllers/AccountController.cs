f89aea057c6cc3ee188cc6144d848f610f9088be
Phil Van Houten <pvanhouten@gmail.com>
2013-08-10 16:05:26 -0500

    Fixing build issues

--- crisischeckin/crisicheckinweb/Controllers/AccountController.cs
+++ crisischeckin/crisicheckinweb/Controllers/AccountController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
-using crisicheckinweb.Models;
+using crisicheckinweb.ViewModels;
 using WebMatrix.WebData;
 
 namespace crisicheckinweb.Controllers
