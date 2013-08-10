60bda9d0f877ab85c996642765b2730ff4d92872
Bill Wagner <bill.wagner@srtsolutions.com>
2013-08-10 13:40:48 -0400

    Add Moq, and create data service

---
+++ crisischeckin/Services/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    // This service manages the communication to the Models project
+    // using the live database in production, but it can be mocked
+    // for test purposes.
+    internal class DataService
+    {
+
+    }
+}
