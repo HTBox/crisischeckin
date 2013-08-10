c837a9ea2a52bad3a30fa08072638aeff2830c53
Phil Van Houten <pvanhouten@gmail.com>
2013-08-10 14:36:10 -0500

    Adding seed data

--- crisischeckin/Models/Migrations/Configuration.cs
+++ crisischeckin/Models/Migrations/Configuration.cs
@@ -26,6 +26,27 @@ namespace Models.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            context.Disasters.AddOrUpdate(d => d.Id,
+                new Disaster { Id = 1, Name = "Fake Disaster 1" },
+                new Disaster { Id = 2, Name = "Fake Disaster 2" }
+            );
+
+            context.Users.AddOrUpdate(u => u.Id,
+                new User { Id = 1, UserName = "ctester" }
+            );
+
+            context.Persons.AddOrUpdate(p => p.Id,
+                new Person { Id = 1, UserId = 1, FirstName = "Chester", LastName = "Tester", Email = "ctester@test.com", PhoneNumber = "608-555-1212"},
+                new Person { Id = 2, FirstName = "Lester", LastName = "Tester", Email = "ltester@test.com", PhoneNumber = "608-555-1212" }
+            );
+
+            context.Commitments.AddOrUpdate(c => c.Id, 
+                new Commitment { Id = 1, DisasterId = 1, PersonId = 1, StartDate = new DateTime(2013, 8, 10), EndDate = new DateTime(2013, 8, 14) },
+                new Commitment { Id = 2, DisasterId = 1, PersonId = 2, StartDate = new DateTime(2013, 8, 12), EndDate = new DateTime(2013, 8, 15) },
+                new Commitment { Id = 3, DisasterId = 2, PersonId = 1, StartDate = new DateTime(2013, 9, 1), EndDate = new DateTime(2013, 9, 5) }
+            );
+
         }
     }
 }
