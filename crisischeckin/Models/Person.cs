17a9cae0ed7355c5de323c994c37a5dc84e45655
dotnetkow <matt.netkow@gmail.com>
2013-08-10 13:04:11 -0500

    Updating Person interface.

--- crisischeckin/Models/Person.cs
+++ crisischeckin/Models/Person.cs
@@ -9,7 +9,7 @@ namespace Models
     public class Person
     {
         public int Id { get; set; }         // Used for identity in Person table.
-        public int UserId { get; set; }     // Used for membership provider and eventually an OAuth implementation
+        public int? UserId { get; set; }     // Used for membership provider and eventually an OAuth implementation
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
--- crisischeckin/Services/VolunteerService.cs
+++ crisischeckin/Services/VolunteerService.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
 using Services.Interfaces;
 
 namespace Services
 {
     public class VolunteerService : IVolunteer
     {
-        public bool Register(string firstName, string lastName, string email, string phoneNumber)
+        public Person Register(string firstName, string lastName, string email, string phoneNumber)
         {
             if (string.IsNullOrEmpty(firstName)) { throw new ArgumentNullException("firstName"); }
             if (string.IsNullOrEmpty(lastName)) { throw new ArgumentNullException("lastName"); }
@@ -18,7 +19,12 @@ namespace Services
 
             // TODO: call into DB using entity framework
 
-            return true;
+            Person person = new Person() 
+            { 
+                Id = 1, UserId = null, FirstName = "Bob", LastName = "jones", Email = "bob.jones@email.com", PhoneNumber = "555-333-1111" 
+            };
+
+            return person;
         }
     }
 }
