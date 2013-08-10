32e334eae2bc189754ca17bda06caecab013d9c4
dotnetkow <matt.netkow@gmail.com>
2013-08-10 11:08:20 -0500

    Added Basic Volunteer model/controller

---
+++ crisischeckin/crisicheckinweb/Models/VolunteerUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crisicheckinweb.Models
+{
+    public class VolunteerUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
