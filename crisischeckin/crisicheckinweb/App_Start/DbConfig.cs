1139a9d5224923df41d0c959c9f2896370353381
Phil Van Houten <pvanhouten@gmail.com>
2013-08-10 14:09:12 -0500

    Updating project to remove old models, setting up database migrations to run on app start

--- crisischeckin/crisicheckinweb/App_Start/DbConfig.cs
+++ crisischeckin/crisicheckinweb/App_Start/DbConfig.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Data.Entity;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +11,8 @@ namespace crisicheckinweb
     {
         public static void Initialize()
         {
-            
+            Database.SetInitializer<CrisisCheckin>(
+                new MigrateDatabaseToLatestVersion<CrisisCheckin, Models.Migrations.Configuration>());
         }
     }
 }
