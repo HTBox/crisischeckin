0ab0b1346606021a2f8149cdc1768786898b9f1b
dotnetkow <matt.netkow@gmail.com>
2013-08-10 11:44:12 -0500

    Added volunteer interface.

---
+++ crisischeckin/Services/Interfaces/IVolunteer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IVolunteer
+    {
+        bool Register(string firstName, string lastName, string email, string phoneNumber);
+
+
+    }
+}
---
+++ crisischeckin/Services/VolunteerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.Interfaces;
+
+namespace Services
+{
+    public class VolunteerService : IVolunteer
+    {
+        public bool Register(string firstName, string lastName, string email, string phoneNumber)
+        {
+            if (string.IsNullOrEmpty(firstName)) { throw new ArgumentNullException("firstName"); }
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
