16541b35848a2bce73c69058a0d0f8d61db244f7
Tim Hoffman <tjhoffman1@gmail.com>
2013-08-10 11:35:04 -0500

    Adding new controllers and views

---
+++ crisischeckin/crisicheckinweb/Controllers/DisasterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace crisicheckinweb.Controllers
+{
+    public class DisasterController : Controller
+    {
+        //
+        // GET: /Disaster/
+
+        public ActionResult List()
+        {
+            return View();
+        }
+        
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ crisischeckin/crisicheckinweb/Controllers/VolunteerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace crisicheckinweb.Controllers
+{
+    public class VolunteerController : Controller
+    {
+        //
+        // GET: /Volunteer/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
