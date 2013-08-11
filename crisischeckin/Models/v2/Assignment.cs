20d3fe22957cb95a4472e7753b292c336fa78f1d
wolters <scott.wolter@gmail.com>
2013-08-11 09:52:12 -0500

    Added models for "V2" features.

---
+++ crisischeckin/Models/v2/Assignment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.v2
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public int LocationId { get; set; }
+        public int PersonId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+    }
+}
---
+++ crisischeckin/Models/v2/PersonPhone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.v2
+{
+    public class PersonPhone
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public string PhoneNumber { get; set; }
+        public int PhoneTypeId { get; set; }
+
+    }
+}
