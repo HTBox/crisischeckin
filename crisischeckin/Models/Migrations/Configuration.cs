e77ab99785f428c9620c0ebcac43eec3cfaefc97
Bill Wagner <bill.wagner@srtsolutions.com>
2013-08-11 11:04:47 -0500

    scaffold initial database creation migration
    Run the package manager console command to create a migration

--- crisischeckin/Models/Migrations/Configuration.cs
+++ crisischeckin/Models/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace Models.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Models.CrisisCheckin>
+    public sealed class Configuration : DbMigrationsConfiguration<Models.CrisisCheckin>
     {
         public Configuration()
         {
