9ecfc7ceccd0d2c76d55baf65932e9400658d591
jojocode <subscriptions-pn@hotmail.com>
2013-10-05 15:53:35 -0500

    Add CheckPage

--- crisischeckin/mobile/winphone/CrisisCheckin/CrisisCheckinApp/MainPage.xaml.cs
+++ crisischeckin/mobile/winphone/CrisisCheckin/CrisisCheckinApp/MainPage.xaml.cs
@@ -7,11 +7,15 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using CrisisCheckinApp.ServiceClient;
+using CrisisCheckinApp.ViewModels;
 
 namespace CrisisCheckinApp
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        public static Disaster CurrDisaster;
+
         // Constructor
         public MainPage()
         {
@@ -28,6 +32,40 @@ namespace CrisisCheckinApp
             {
                 App.ViewModel.LoadData();
             }
+        }
+
+        private void TextBlock_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void Disaster_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            // open new checkin page
+            NavigationService.Navigate(new Uri("/CheckinPage.xaml", UriKind.Relative));
+
+
+        }
+
+        private void LongListSelector_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+        }
+
+        private void OngoingDisasters_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            // open new checkin page
+            
+            
+
+
+        }
+
+        private void OngoingDisaster_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            CurrDisaster = ((sender as TextBlock).DataContext as DisasterViewModel).Disaster;
+            NavigationService.Navigate(new Uri("/CheckinPage.xaml", UriKind.Relative));
+
         }
     }
 }
--- crisischeckin/mobile/winphone/CrisisCheckin/CrisisCheckinApp/ViewModels/MainViewModel.cs
+++ crisischeckin/mobile/winphone/CrisisCheckin/CrisisCheckinApp/ViewModels/MainViewModel.cs
@@ -77,24 +77,7 @@ namespace CrisisCheckinApp.ViewModels
             {
                 this.OngoingDisasters.Add(new DisasterViewModel { Disaster = od });
             }
-             
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime one", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime two", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime three", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime four", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime five", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime six", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime seven", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime eight", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime nine", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime ten", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime eleven", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime twelve", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime thirteen", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime fourteen", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime fifteen", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat" });
-            this.Items.Add(new ItemViewModel() { LineOne = "runtime sixteen", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum" });
-
+          
             this.IsDataLoaded = true;
         }
 
