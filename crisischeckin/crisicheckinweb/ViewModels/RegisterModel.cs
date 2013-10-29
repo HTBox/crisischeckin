6bfa4443714e81cb5813668ddc9851f2f30dd531
volkanik <volkan@me.com>
2013-10-29 13:01:58 -0700

    Fix for Issue #95
    Email address validation added to the model ad data annotation

--- crisischeckin/crisicheckinweb/ViewModels/RegisterModel.cs
+++ crisischeckin/crisicheckinweb/ViewModels/RegisterModel.cs
@@ -23,6 +23,7 @@ namespace crisicheckinweb.ViewModels
 
         [Required]
         [Display(Name = "Email")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
 
         [Required]
