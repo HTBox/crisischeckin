536958e8e64b2cd869fa71ab88e20c55d46bf93e
Gad Berger <gad.berger@gmail.com>
2013-11-08 21:29:57 -0500

    adding an interface for the static web security class
    These classes will make it possible to mock and test the controllers
    that rely on the static properties in WebSecurity

---
+++ crisischeckin/crisicheckinweb/Wrappers/IWebSecurityWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebMatrix.WebData;
+
+namespace crisicheckinweb.Wrappers
+{
+    public interface IWebSecurityWrapper
+    {
+        int CurrentUserId { get; }
+    }
+}
