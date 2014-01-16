8820e661a8ba8f5d8ed152f4eca981ae4d836887
Kevin Stites <particle6@gmail.com>
2014-01-16 13:40:16 -0800

    Added additional optional parameters for RouteValues and HtmlAttributes on the helper

--- crisischeckin/crisicheckinweb/Helpers/MenuExtensions.cs
+++ crisischeckin/crisicheckinweb/Helpers/MenuExtensions.cs
@@ -7,18 +7,35 @@ using System.Web.Mvc.Html;
 
 public static class MenuExtensions
 {
-    public static MvcHtmlString MenuItem(this HtmlHelper htmlHelper, string text, string action, string controller)
+    public static MvcHtmlString MenuItem(this HtmlHelper htmlHelper, string text, string action, string controller, object routeValues = null, object htmlAttributes = null)
     {
         var li = new TagBuilder("li");
         var routeData = htmlHelper.ViewContext.RouteData;
         var currentAction = routeData.GetRequiredString("action");
         var currentController = routeData.GetRequiredString("controller");
-        if (string.Equals(currentAction, action, StringComparison.OrdinalIgnoreCase) &&
-            string.Equals(currentController, controller, StringComparison.OrdinalIgnoreCase))
+        if (string.Equals(currentAction, action, StringComparison.OrdinalIgnoreCase) || string.Equals(currentController, controller, StringComparison.OrdinalIgnoreCase))
         {
             li.AddCssClass("active");
         }
-        li.InnerHtml = htmlHelper.ActionLink(text, action, controller).ToHtmlString();
+        if (routeValues != null)
+        {
+            li.InnerHtml = (htmlAttributes != null)
+                ? htmlHelper.ActionLink(text,
+                                        action,
+                                        controller,
+                                        routeValues,
+                                        htmlAttributes).ToHtmlString()
+                : htmlHelper.ActionLink(text,
+                                        action,
+                                        controller,
+                                        routeValues).ToHtmlString();
+        }
+        else
+        {
+            li.InnerHtml = htmlHelper.ActionLink(text,
+                                                 action,
+                                                 controller).ToHtmlString();
+        }
         return MvcHtmlString.Create(li.ToString());
     }
 }
