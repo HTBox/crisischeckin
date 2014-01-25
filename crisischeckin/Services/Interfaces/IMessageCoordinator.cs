1eb02e653fa0ebfa12b0a0eab53d37fea4956c11
James Jensen <j2jensen@gmail.com>
2014-01-25 16:06:32 -0700

    Implemented infrastructure for message coordinator

---
+++ crisischeckin/Services/Interfaces/IMessageCoordinator.cs
@@ -0,0 +1,7 @@
+﻿namespace Services.Interfaces
+{
+    public interface IMessageCoordinator
+    {
+        
+    }
+}
---
+++ crisischeckin/Services/MessageCoordinator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Services.Interfaces;
+
+namespace Services
+{
+    public class MessageCoordinator : IMessageCoordinator
+    {
+        private readonly List<IMessageSender> _messageSenders;
+
+        public MessageCoordinator(List<IMessageSender> messageSenders)
+        {
+            _messageSenders = messageSenders;
+        }
+
+        public void SendMessage(Message message, List<MessageRecipient> recipients)
+        {
+            foreach (var messageSender in _messageSenders)
+            {
+                try
+                {
+                    messageSender.SendMessage(message, recipients);
+                }
+                catch (Exception e)
+                {
+                    //TODO: Add logging implementation--awaiting decision from Bill
+                }
+            }
+        }
+    }
+}
