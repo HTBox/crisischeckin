7e30d9b8e26f08f54883715a21a65b32b63a0fb6
david-adsit <david-adsit@pluralsight.com>
2014-01-25 13:34:07 -0700

    The first Acceptance Test fails for the right reason

--- crisischeckin/AcceptanceTests/ClusterCoordinatorFeature/A_person_can_be_assigned_as_a_cluster_coordinator_for_a_disaster.cs
+++ crisischeckin/AcceptanceTests/ClusterCoordinatorFeature/A_person_can_be_assigned_as_a_cluster_coordinator_for_a_disaster.cs
@@ -9,10 +9,11 @@ namespace AcceptanceTests.ClusterCoordinatorFeature
     [TestClass]
     public class A_person_can_be_assigned_as_a_cluster_coordinator_for_a_disaster : With_an_empty_database_environment
     {
-        const int ClusterId = 5;
+         int _clusterId = 5;
         const int UserId = 100;
         Disaster _disaster;
         Person _person;
+        DataService _dataService;
 
         [TestInitialize]
         public void Arrange()
@@ -25,18 +26,19 @@ namespace AcceptanceTests.ClusterCoordinatorFeature
         public void Assign_a_user_and_verify_results()
         {
             IClusterCoordinatorService clusterCoordinatorService = new ClusterCoordinatorService();
-            clusterCoordinatorService.AssignClusterCoordinator(_disaster.Id, ClusterId, _person.Id);
+            clusterCoordinatorService.AssignClusterCoordinator(_disaster.Id, _clusterId, _person.Id);
 
             var clusterCoordinators = clusterCoordinatorService.GetAllCoordinators(_disaster.Id);
 
-            Assert.IsTrue(clusterCoordinators.Any(c => c.DisasterId == _disaster.Id && c.ClusterId == ClusterId && c.PersonId == _person.Id));
+            Assert.IsTrue(clusterCoordinators.Any(c => c.DisasterId == _disaster.Id && c.ClusterId == _clusterId && c.PersonId == _person.Id));
             //Assert that the log contains an assigned event
         }
 
         void Create_a_person()
         {
-            var volunteerService = new VolunteerService(new DataService(new CrisisCheckin()));
-            _person = volunteerService.Register("Rob", "Lowe", "rob@lowe.com", "890-1230-4567", ClusterId, UserId);
+            _dataService = new DataService(new CrisisCheckin());
+            var volunteerService = new VolunteerService(_dataService);
+            _person = volunteerService.Register("Rob", "Lowe", "rob@lowe.com", "890-1230-4567", _clusterId, UserId);
         }
 
         void Create_a_disaster()
@@ -46,6 +48,7 @@ namespace AcceptanceTests.ClusterCoordinatorFeature
                             IsActive = true,
                             Name = "Great Seattle Starbucks Strike",
                         };
+            _clusterId = _dataService.Clusters.First().Id;
         }
     }
 }
---
+++ crisischeckin/AcceptanceTests/With_an_empty_database_environment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+
+namespace AcceptanceTests
+{
+    [TestClass]
+    public class With_an_empty_database_environment
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Directory.GetCurrentDirectory());
+            Database.SetInitializer<CrisisCheckin>(new DropCreateDatabaseAlways<CrisisCheckin>());
+
+            using (var context = new CrisisCheckin())
+            {
+                if (!context.Database.Exists())
+                {
+                    ((IObjectContextAdapter) context).ObjectContext.CreateDatabase();
+                }
+            }
+        }
+    }
+}
