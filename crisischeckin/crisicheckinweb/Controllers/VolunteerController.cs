7afe7eaedef31249c5917e684ab4d24fddd2d7f7
Chad Queen <chaddolan82@gmail.com>
2014-01-25 12:06:59 -0700

    Added interface for building a message to volunteers

--- crisischeckin/crisicheckinweb/Controllers/VolunteerController.cs
+++ crisischeckin/crisicheckinweb/Controllers/VolunteerController.cs
@@ -11,10 +11,12 @@ namespace crisicheckinweb.Controllers
     public class VolunteerController : BaseController
     {
         private IDisaster _disasterSvc;
+        private ICluster _clusterSvc;
         private IAdmin _adminSvc;
-        public VolunteerController(IDisaster disasterSvc, IAdmin adminSvc)
+        public VolunteerController(IDisaster disasterSvc, ICluster clusterService, IAdmin adminSvc)
         {
             _disasterSvc = disasterSvc;
+            _clusterSvc = clusterService;
             _adminSvc = adminSvc;
         }
 
@@ -22,6 +24,37 @@ namespace crisicheckinweb.Controllers
         public ActionResult ListByDisaster()
         {
             var model = new ListByDisasterViewModel { Disasters = _disasterSvc.GetActiveList() };
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult CreateMessageToVolunteersByDisaster(int id)
+        {
+            ViewBag.Clusters = _clusterSvc.GetList();
+            var model = new SendMessageToAllVolunteersByDisasterViewModel { DisasterId = id };
+            this.PopulateSendMessageViewModel(model);
+            // Default subject to disaster name so they can type just a message if they want to.
+            model.Subject = model.DisasterName;
+            return View("CreateMessage", model);
+        }
+
+        private void PopulateSendMessageViewModel(SendMessageToAllVolunteersByDisasterViewModel model)
+        {
+            var disaster = _disasterSvc.Get(model.DisasterId);
+            model.DisasterName = disaster.Name;
+            model.Clusters = this._clusterSvc.GetList();
+        }
+
+        [HttpPost]
+        public ActionResult SendMessageToVolunteersByDisaster(SendMessageToAllVolunteersByDisasterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                this.PopulateSendMessageViewModel(model);
+                return this.View("CreateMessage", model);
+            }
+            throw new NotImplementedException();
+
             return View(model);
         }
 
