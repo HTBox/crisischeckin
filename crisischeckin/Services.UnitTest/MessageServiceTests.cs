fd81b572104e98ddbd051540c11d763508301756
David Batten <battenworks@live.com>
2014-01-25 17:50:42 -0700

    Implemented MessageCoordinator in the MessageService

--- crisischeckin/Services.UnitTest/MessageServiceTests.cs
+++ crisischeckin/Services.UnitTest/MessageServiceTests.cs
@@ -15,11 +15,20 @@ namespace Services.UnitTest
         {
             var mockAdminSvc = new Mock<IAdmin>();
             var volunteers = new List<Person>();
-            volunteers.Add(new Person { Id = 1, PhoneNumber = "phone-number-1", Email = "email-1" });
-            volunteers.Add(new Person { Id = 2, PhoneNumber = "phone-number-2", Email = "email-2" });
+            volunteers.Add(new Person { Id = 1, Email = "email-1", FirstName = "first", LastName = "last" });
+            volunteers.Add(new Person { Id = 2, Email = "email-2", FirstName = "first", LastName = "last" });
             mockAdminSvc.Setup(x => x.GetVolunteersForDate(It.IsAny<int>(), DateTime.Today)).Returns(volunteers);
             var mockMessageCoordinator = new Mock<IMessageCoordinator>();
 
+            var expectedMessage = new Message("body", "subject");
+            var expectedRecipients = new List<MessageRecipient>();
+            expectedRecipients.Add(new MessageRecipient { EmailAddress = "email-1", Name = "first last" });
+            expectedRecipients.Add(new MessageRecipient { EmailAddress = "email-2", Name = "first last" });
+
+            var sut = new MessageService(mockAdminSvc.Object, mockMessageCoordinator.Object);
+            sut.SendMessageToDisasterVolunteers(new RecipientCriterion(42), expectedMessage);
+
+            mockMessageCoordinator.Verify(x => x.SendMessage(expectedMessage, It.Is<List<MessageRecipient>>(y => y.Count.Equals(2))), Times.Once());
         }
     }
 }
