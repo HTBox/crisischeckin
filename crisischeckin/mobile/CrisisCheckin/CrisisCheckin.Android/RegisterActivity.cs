88d4eab7cf941de14ad845c47ffbada2e72dba22
Redth <jondick@gmail.com>
2014-04-25 22:03:52 -0400

    Wired up spinner on Android Register Activity

--- crisischeckin/mobile/CrisisCheckin/CrisisCheckin.Android/RegisterActivity.cs
+++ crisischeckin/mobile/CrisisCheckin/CrisisCheckin.Android/RegisterActivity.cs
@@ -10,12 +10,16 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Threading.Tasks;
+using CrisisCheckin.Core;
 
 namespace CrisisCheckin.Android
 {
 	[Activity (Label = "RegisterActivity")]			
 	public class RegisterActivity : Activity
 	{
+		IWebService webService;
+
 		EditText editTextFirstName;
 		EditText editTextLastName;
 		EditText editTextEmail;
@@ -24,12 +28,15 @@ namespace CrisisCheckin.Android
 		EditText editTextPassword;
 		EditText editTextPassword2;
 		Spinner spinnerCluster;
+		Button buttonCreate;
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
 			SetContentView (Resource.Layout.Register);
+
+			webService = WebServiceFactory.Create ();
 
 			editTextFirstName = FindViewById<EditText> (Resource.Id.editTextFirstName);
 			editTextLastName = FindViewById<EditText> (Resource.Id.editTextLastName);
@@ -38,6 +45,32 @@ namespace CrisisCheckin.Android
 			editTextUsername = FindViewById<EditText> (Resource.Id.editTextUsername);
 			editTextPassword = FindViewById<EditText> (Resource.Id.editTextPassword);
 			editTextPassword2 = FindViewById<EditText> (Resource.Id.editTextPassword2);
+			spinnerCluster = FindViewById<Spinner> (Resource.Id.spinnerCluster);
+			buttonCreate = FindViewById<Button> (Resource.Id.buttonCreate);
+
+			//Let's disable the button until the clusters have loaded
+			buttonCreate.Enabled = false;
+
+			Refresh ();
+		}
+
+		async Task Refresh()
+		{
+			var r = await webService.GetClustersAsync(new ClustersRequest ());
+
+			if (!r.Succeeded) {
+				buttonCreate.Enabled = false;
+				Toast.MakeText (this, "Failed to Fetch Clusters.  Please try again later.", ToastLength.Short).Show ();
+				return;
+			}
+
+			if (r.Result.Any ()) {
+
+				var items = (from c in r.Result select c.Name).ToArray();
+
+				spinnerCluster.Adapter = new ArrayAdapter<string> (this, global::Android.Resource.Layout.SimpleSpinnerItem, items);
+				buttonCreate.Enabled = true;
+			}
 		}
 	}
 }
