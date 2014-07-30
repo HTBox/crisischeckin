3d12341f06c941f50ca36a3abcd3d00f369871bd
Redth <jondick@gmail.com>
2014-07-30 09:43:33 -0400

    [mobile] iOS not using storyboards, some implementation now done

--- crisischeckin/mobile/CrisisCheckin/CrisisCheckin.IOS/AppDelegate.cs
+++ crisischeckin/mobile/CrisisCheckin/CrisisCheckin.IOS/AppDelegate.cs
@@ -3,27 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using CrisisCheckin.Shared;
 
 namespace CrisisCheckin
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the
-	// User Interface of the application, as well as listening (and optionally responding) to
-	// application events from iOS.
-
-
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : UIApplicationDelegate
 	{
-		public override UIWindow Window { get; set; }
+		UIWindow window;
+		UINavigationController navigationController;
+		SignInViewController signInViewController;
 
-		// This method is invoked when the application has loaded and is ready to run. In this
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
+		public static Settings Settings { get; private set; }
+
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
+			Settings = Settings.Load();
+
+			signInViewController = new SignInViewController ();
+			navigationController = new UINavigationController (signInViewController);
+
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+			window.RootViewController = navigationController;
+			window.MakeKeyAndVisible ();
+
 			return true;
 		}
 	}
---
+++ crisischeckin/mobile/CrisisCheckin/CrisisCheckin.IOS/RegisterViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MonoTouch.Dialog;
+using MonoTouch.UIKit;
+
+namespace CrisisCheckin
+{
+	public class RegisterViewController : DialogViewController
+	{
+		public RegisterViewController () : 
+			base (UITableViewStyle.Grouped, new RootElement ("Register"), true)
+		{
+			Root = new RootElement ("Register") {
+				new Section {
+					new EntryElement ("First Name", "", ""),
+					new EntryElement ("Last Name", "", ""),
+					new EntryElement ("Phone", "123-123-1234", "") {
+						KeyboardType = UIKeyboardType.PhonePad
+					},
+					new EntryElement ("Email", "your@email.com", "") {
+
+					},
+					new EntryElement ("Username", "username", "") {
+
+					},
+					new EntryElement ("Password", "password", "") {
+
+					},
+					new EntryElement ("Confirm", "password", "") {
+
+					},
+				}
+			};
+		}
+	}
+}
+
