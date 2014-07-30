88ca61feab88677da4e9d5f88f25af7c810e3c4a
Redth <jondick@gmail.com>
2014-07-29 20:12:48 -0400

    [mobile] Fixed up iOS project

--- crisischeckin/mobile/CrisisCheckin/CrisisCheckin.IOS/AppDelegate.cs
+++ crisischeckin/mobile/CrisisCheckin/CrisisCheckin.IOS/AppDelegate.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
-namespace CrisisCheckin.IOS
+namespace CrisisCheckin
 {
 	// The UIApplicationDelegate for the application. This class is responsible for launching the
 	// User Interface of the application, as well as listening (and optionally responding) to
@@ -14,11 +14,8 @@ namespace CrisisCheckin.IOS
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : UIApplicationDelegate
 	{
-		// class-level declarations
-		UIWindow window;
-		CrisisCheckin_IOSViewController controller;
-		//
-		UINavigationController navcontroller;
+		public override UIWindow Window { get; set; }
+
 		// This method is invoked when the application has loaded and is ready to run. In this
 		// method you should instantiate the window, load the UI into it and then make the window
 		// visible.
@@ -27,27 +24,7 @@ namespace CrisisCheckin.IOS
 		//
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
-			window = new UIWindow (UIScreen.MainScreen.Bounds);
-			controller = new CrisisCheckin_IOSViewController ();
-			var initialControllers = new List<UIViewController>();
-			initialControllers.Add (controller);
-
-			navcontroller = new UINavigationController ();
-			navcontroller.ViewControllers = initialControllers.ToArray ();
-			window.RootViewController = navcontroller;
-			window.MakeKeyAndVisible ();
-			UIHelper.Present (new LoginViewController ());
 			return true;
-		}
-		public override void WillEnterForeground (UIApplication application)
-		{
-			if (Locker.Locked) {
-				UIHelper.Present (new LoginViewController ());
-			}
-		}
-		public override void DidEnterBackground (UIApplication application)
-		{
-			Locker.Locked = true;
 		}
 	}
 }
