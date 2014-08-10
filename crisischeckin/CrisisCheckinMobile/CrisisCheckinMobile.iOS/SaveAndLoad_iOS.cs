8115033e3129af1887d2080ca3194368811814cc
menewman <michaeleugenenewman@gmail.com>
2014-08-10 16:02:14 -0500

    Added SaveAndLoad implementation for iOS

---
+++ crisischeckin/CrisisCheckinMobile/CrisisCheckinMobile.iOS/SaveAndLoad_iOS.cs
@@ -0,0 +1,28 @@
+using CrisisCheckinMobile.iOS;
+using CrisisCheckinMobile.SaveLoad;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency (typeof (SaveAndLoad_iOS))]
+namespace CrisisCheckinMobile.iOS
+{
+	public class SaveAndLoad_iOS : ISaveAndLoad
+	{
+		public SaveAndLoad_iOS() {}
+
+		public void SaveText(string filename, string text)
+		{
+			var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			var filePath = Path.Combine(documentsPath, filename);
+			System.IO.File.WriteAllText(filePath, text);
+		}
+
+		public string LoadText(string filename)
+		{
+			var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			var filePath = Path.Combine(documentsPath, filename);
+			return System.IO.File.ReadAllText(filePath);
+		}
+	}
+}
