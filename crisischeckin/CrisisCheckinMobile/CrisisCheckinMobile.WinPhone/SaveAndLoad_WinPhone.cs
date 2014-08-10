8b074652ad8c49b73185ba4dd96bf996cf1a4887
menewman <michaeleugenenewman@gmail.com>
2014-08-10 16:29:40 -0500

    Added (still-not-implemented) SaveAndLoad file for WinPhone

---
+++ crisischeckin/CrisisCheckinMobile/CrisisCheckinMobile.WinPhone/SaveAndLoad_WinPhone.cs
@@ -0,0 +1,23 @@
+﻿using CrisisCheckinMobile.WinPhone;
+using CrisisCheckinMobile.SaveLoad;
+using System;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SaveAndLoad_WinPhone))]
+namespace CrisisCheckinMobile.WinPhone
+{
+	public class SaveAndLoad_WinPhone : ISaveAndLoad
+	{
+		public SaveAndLoad_WinPhone() { }
+
+		public void SaveText(string filename, string text)
+		{
+			throw new NotImplementedException("Saving files not yet supported for Windows Phone");
+		}
+
+		public string LoadText(string filename)
+		{
+			throw new NotImplementedException("Loading files not yet supported for Windows Phone");
+		}
+	}
+}
