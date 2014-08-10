f687d4c8537b4ca2cd607ab831587808a377792a
Edward Brey <edward.brey@toolsay.com>
2014-08-10 15:20:02 -0500

    added client models

--- crisischeckin/CrisisCheckinMobile/CrisisCheckinMobile/App.cs
+++ crisischeckin/CrisisCheckinMobile/CrisisCheckinMobile/App.cs
@@ -9,30 +9,30 @@ namespace CrisisCheckinMobile
 {
     public class App
     {
-		public static string AuthToken { get; set; }
-		public static Committment Committment { get; set; }
-		public static string BackIcon = "";
-		public static string ReportTroubleIcon = "";
-		public static string ProfileIcon = "";
-		public static Color ContentBackgroundColor = Color.FromHex ("4f2c1d");
-		public static Color MastHeadBackgroundColor = Color.FromHex ("d22630");
-		public static Color LowLightColor = Color.FromHex ("ceb888");
-		public static Color TextColor = Color.White;
+        public static string AuthToken { get; set; }
+        public static Commitment Commitment { get; set; }
+        public static string BackIcon = "";
+        public static string ReportTroubleIcon = "";
+        public static string ProfileIcon = "";
+        public static readonly Color ContentBackgroundColor = Color.FromHex("4f2c1d");
+        public static readonly Color MastHeadBackgroundColor = Color.FromHex("d22630");
+        public static readonly Color LowLightColor = Color.FromHex("ceb888");
+        public static readonly Color TextColor = Color.White;
 
         public static Page GetMainPage()
         {
-			NavigationPage mainPage;
-			if (string.IsNullOrWhiteSpace (AuthToken)) {
-				mainPage = new NavigationPage (new LoginPage ());
-			} else {
-				mainPage = new NavigationPage (new DisasterListPage ());
-			}
+            NavigationPage mainPage;
+            if (string.IsNullOrWhiteSpace(AuthToken)) {
+                mainPage = new NavigationPage(new LoginPage());
+            } else {
+                mainPage = new NavigationPage(new DisasterListPage());
+            }
 
-			if (Committment != null) {
-				mainPage.Navigation.PushAsync (new DisasterCommittmentPage (Committment));
-			}
+            if (Commitment != null) {
+                mainPage.Navigation.PushAsync(new CommitmentPage());
+            }
 
-			return mainPage;
+            return mainPage;
         }
 
 
---
+++ crisischeckin/CrisisCheckinMobile/CrisisCheckinMobile/CommitmentPage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CrisisCheckinMobile
+{
+    public class CommitmentPage : ContentPage
+    {
+        public CommitmentPage()
+        {
+            var grid = new Grid() {
+                RowDefinitions = new RowDefinitionCollection {
+                    new RowDefinition { },
+                    new RowDefinition { },
+                }
+            };
+
+            var plannedLabel = new Label {
+                Text = "I am"
+            };
+            plannedLabel.SetValue(Grid.RowProperty, 0);
+
+            grid.Children.Add(plannedLabel);
+        }
+    }
+}
