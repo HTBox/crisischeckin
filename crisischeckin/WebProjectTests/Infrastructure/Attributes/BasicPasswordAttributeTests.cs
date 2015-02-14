98592b0260833f58707dc1d10b1a0365c36947f5
Asher Wildeboer <asherw83@gmail.com>
2015-02-14 11:17:23 +1000

    Create new BasicPassword attribute. Apply to register and update password models. Add unit tests.

---
+++ crisischeckin/WebProjectTests/Infrastructure/Attributes/BasicPasswordAttributeTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using crisicheckinweb.Infrastructure.Attributes;
+
+namespace WebProjectTests.Infrastructure.Attributes
+{
+    [TestClass]
+    public class BasicPasswordAttributeTests
+    {
+        private BasicPasswordAttribute Model;
+
+        public BasicPasswordAttributeTests()
+        {
+            Model = new BasicPasswordAttribute();
+        }
+
+        /// <summary>
+        /// Allows for the attribute to be placed on a form, but not enforcing entry.
+        /// </summary>
+        [TestMethod]
+        public void BlankPasswordValueIsOk()
+        {
+            Assert.IsTrue(Model.IsValid(string.Empty));
+        }
+
+        [TestMethod]
+        public void RegularInputPasses()
+        {
+            Assert.IsTrue(Model.IsValid("P@55w0rd"));
+        }
+
+        [TestMethod]
+        public void PasswordsWithOutsideCharactersPass()
+        {
+            Assert.IsTrue(Model.IsValid("PÆsswordb1"));
+        }
+
+        [TestMethod]
+        public void GenericPasswordFails()
+        {
+            Assert.IsFalse(Model.IsValid("password"));
+        }
+
+        [TestMethod]
+        public void ShortPasswordsFail()
+        {
+            Assert.IsFalse(Model.IsValid("short"));
+        }
+
+        [TestMethod]
+        public void LongPasswordsFail()
+        {
+            Assert.IsFalse(Model.IsValid("ThisPasswordIsALittleLong"));
+        }
+
+    }
+}
---
+++ crisischeckin/crisicheckinweb/Infrastructure/Attributes/BasicPasswordAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Resources;
+
+namespace crisicheckinweb.Infrastructure.Attributes
+{
+    public class BasicPasswordAttribute : RegularExpressionAttribute
+    {
+        public BasicPasswordAttribute()
+            : base(@"^[^\s]{6,20}$")
+        {
+            ErrorMessage = DefaultErrorMessages.InvalidPasswordFormat;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (!base.IsValid(value)) return false;
+
+            var commonWords = new[] { "password", "pass", "ht" };
+            var stringValue = (string)value;
+
+            // Some fields are not required
+            if (string.IsNullOrWhiteSpace(stringValue)) return true;
+
+            if (commonWords.Any(w => stringValue.IndexOf(w, StringComparison.InvariantCultureIgnoreCase) >= 0))
+            {
+                ErrorMessage = DefaultErrorMessages.InvalidPasswordGenericUsed;
+                return false;
+            }
+
+            return true;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (!this.IsValid(value))
+                return new ValidationResult(ErrorMessage);
+
+            return null;
+        }
+    }
+}
