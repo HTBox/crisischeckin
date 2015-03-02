a60b780d4122f62df0d0474aac06363aea8d5cf9
Taco D <taco@tdev.org>
2015-03-03 00:19:05 +0100

    Added unit tests to cover username != password check.

---
+++ crisischeckin/WebProjectTests/Infrastructure/PasswordComplexityTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using crisicheckinweb.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Resources;
+
+namespace WebProjectTests.Infrastructure
+{
+    [TestClass]
+    public class PasswordComplexityTests
+    {
+        [TestMethod]
+        public void ValidWhenPasswordIsEqualToUsername()
+        {
+            // Act
+            string errorMessage;
+            bool valid = PasswordComplexity.IsValid("psswrd", "user01", out errorMessage);
+
+            // Assert
+            Assert.IsTrue(valid);
+            Assert.IsNull(errorMessage);
+        }
+
+        [TestMethod]
+        public void InvalidWhenPasswordIsEqualToUsername()
+        {
+            // Act
+            string errorMessage;
+            bool valid = PasswordComplexity.IsValid("user01", "user01", out errorMessage);
+
+            // Assert
+            Assert.IsFalse(valid);
+            Assert.AreEqual(DefaultErrorMessages.InvalidPasswordEqualToUserName, errorMessage);
+        }
+    }
+}
