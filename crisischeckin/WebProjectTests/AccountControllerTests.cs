6a3380800703614be52ed59c975ed684453c4eb9
Taco D <taco@tdev.org>
2015-03-03 23:10:21 +0100

    Code cleanup of the Controller tests.

--- crisischeckin/WebProjectTests/AccountControllerTests.cs
+++ crisischeckin/WebProjectTests/AccountControllerTests.cs
@@ -1,5 +1,4 @@
 ﻿using Moq;
-using Common;
 using Models;
 using crisicheckinweb.Controllers;
 using crisicheckinweb.ViewModels;
@@ -17,45 +16,43 @@ namespace WebProjectTests
     [TestClass]
     public class AccountControllerTests
     {
-        private Mock<IVolunteerService> volunteerService;
-        private Mock<ICluster> cluster;
-        private Mock<IWebSecurityWrapper> webSecurityWrapper;
-        private Mock<IPrincipal> principal;
-        private Mock<HttpContextBase> httpContext;
-        private AccountController Controller;
+        private AccountController _controllerUnderTest;
+
+        private Mock<IVolunteerService> _volunteerService;
+        private Mock<ICluster> _cluster;
+        private Mock<IWebSecurityWrapper> _webSecurity;
+        private Mock<IPrincipal> _principal;
+        private Mock<HttpContextBase> _httpContext;
 
         [TestInitialize]
         public void Setup()
         {
-            principal = new Mock<IPrincipal>();
-            httpContext = new Mock<HttpContextBase>();
-            httpContext.Setup(x => x.User).Returns(principal.Object);
+            _principal = new Mock<IPrincipal>();
+            _httpContext = new Mock<HttpContextBase>();
+            _httpContext.Setup(x => x.User).Returns(_principal.Object);
 
-            // Arrange
-            volunteerService = new Mock<IVolunteerService>();
-            cluster = new Mock<ICluster>();
-            webSecurityWrapper = new Mock<IWebSecurityWrapper>();
+            _volunteerService = new Mock<IVolunteerService>();
+            _cluster = new Mock<ICluster>();
+            _webSecurity = new Mock<IWebSecurityWrapper>();
 
-            var reqContext = new RequestContext(httpContext.Object, new RouteData());
+            var reqContext = new RequestContext(_httpContext.Object, new RouteData());
 
-            Controller = new AccountController(volunteerService.Object, cluster.Object, webSecurityWrapper.Object);
-            Controller.ControllerContext = new ControllerContext(reqContext, Controller);
+            _controllerUnderTest = new AccountController(_volunteerService.Object, _cluster.Object, _webSecurity.Object);
+            _controllerUnderTest.ControllerContext = new ControllerContext(reqContext, _controllerUnderTest);
         }
-
-        
 
         [TestMethod]
         public void ChangeContactInfo_Assign_ValidData_Redirects_To_ContactInfoChanged_View()
         {
-            volunteerService.Setup(x => x.FindByUserId(It.IsAny<int>())).Returns(new Person());
-            webSecurityWrapper.SetupGet(x => x.CurrentUserId).Returns(10);
+            // Arrange
+            _volunteerService.Setup(x => x.FindByUserId(It.IsAny<int>())).Returns(new Person());
 
             // Act
             var model = new ChangeContactInfoViewModel { Email = "test@neverEverUsedDomain123141.com", PhoneNumber = "123456789" };
 
-            Mother.ControllerHelpers.SetupControllerModelState(model, Controller);
+            Mother.ControllerHelpers.SetupControllerModelState(model, _controllerUnderTest);
 
-            var response = Controller.ChangeContactInfo(model);
+            var response = _controllerUnderTest.ChangeContactInfo(model);
 
             // Assert
             var view = response as RedirectToRouteResult;
@@ -68,15 +65,17 @@ namespace WebProjectTests
         [TestMethod]
         public void ChangeContactInfo_DuplicateEmailAddress_ReturnsChangeContactInfoView_With_ModelState_Error()
         {
-            volunteerService.Setup(x => x.UpdateDetails(It.IsAny<Person>())).Throws<PersonEmailAlreadyInUseException>();
+            // Arrange
+            _volunteerService.Setup(x => x.UpdateDetails(It.IsAny<Person>())).Throws<PersonEmailAlreadyInUseException>();
 
+
+            // Act
             var model = new ChangeContactInfoViewModel { Email = "test@UsedDomain123141.com", PhoneNumber = "123456789" };
 
-            Mother.ControllerHelpers.SetupControllerModelState(model, Controller);
-            
-            // Act
-            var response = Controller.ChangeContactInfo(model);
-            
+            Mother.ControllerHelpers.SetupControllerModelState(model, _controllerUnderTest);
+
+            var response = _controllerUnderTest.ChangeContactInfo(model);
+
             // Assert
             var result = response as ViewResult;
 
@@ -89,14 +88,14 @@ namespace WebProjectTests
         [TestMethod]
         public void ChangeContactInfo_Invalid_ModelState_Directs_User_To_ChangeContactInfo_View()
         {
-            principal.Object.IsInRole(It.Is<string>(x => x == Constants.RoleAdmin));
+            // Arrange
 
+            // Act
             var model = new ChangeContactInfoViewModel { Email = "test@UsedDomain123141.com", PhoneNumber = "" };
 
-            Mother.ControllerHelpers.SetupControllerModelState(model, Controller);
-            
-            // Act
-            var response = Controller.ChangeContactInfo(model);
+            Mother.ControllerHelpers.SetupControllerModelState(model, _controllerUnderTest);
+
+            var response = _controllerUnderTest.ChangeContactInfo(model);
 
             // Assert
             var result = response as ViewResult;
