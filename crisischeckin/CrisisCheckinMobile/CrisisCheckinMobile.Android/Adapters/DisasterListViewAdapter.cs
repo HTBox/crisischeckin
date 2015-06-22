a264d74e9cf8a62077567c745845ef62a2e3c1f3
Brian Runck <brunck@ngpvan.com>
2015-06-21 20:45:34 -0400

    Android: Added new layout and adapter class

---
+++ crisischeckin/CrisisCheckinMobile/CrisisCheckinMobile.Android/Adapters/DisasterListViewAdapter.cs
@@ -0,0 +1,37 @@
+﻿using Android.Widget;
+using System.Collections.Generic;
+
+namespace CrisisCheckinMobile.Droid
+{
+    public class DisasterListViewAdapter : BaseAdapter<string>
+    {
+        List<string> _items;
+
+        public override long GetItemId(int position)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Android.Views.View GetView(int position, Android.Views.View convertView, Android.Views.ViewGroup parent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int Count
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public override string this[int index]
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
+
