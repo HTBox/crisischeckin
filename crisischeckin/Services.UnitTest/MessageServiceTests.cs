103439caa2fd34df34586f2409c45b2f3402cede
Eric Simons <lodieric@yahoo.com>
2015-08-08 19:57:26 -0500

    Added a unit test: MessageService__DoesNotFail_With_Empty_VolunteerList

--- crisischeckin/Services.UnitTest/MessageServiceTests.cs
+++ crisischeckin/Services.UnitTest/MessageServiceTests.cs
@@ -32,5 +32,26 @@ namespace Services.UnitTest
 
             mockMessageCoordinator.Verify(x => x.SendMessage(expectedMessage, It.Is<List<MessageRecipient>>(y => y.Count.Equals(2)), sender), Times.Once());
         }
+
+        [Test]
+        public void MessageService__DoesNotFail_With_Empty_VolunteerList()
+        {
+            const string sender = "Sender Name";
+
+            var mockAdminSvc = new Mock<IAdmin>();
+            var volunteers = new List<Person>();
+            volunteers.Add(new Person { Id = 1, Email = "email-1", FirstName = "first", LastName = "last" });
+            volunteers.Add(new Person { Id = 2, Email = "email-2", FirstName = "first", LastName = "last" });
+            mockAdminSvc.Setup(x => x.GetVolunteersForDate(It.IsAny<int>(), DateTime.Today, false, false)).Returns(volunteers);
+            var mockMessageCoordinator = new Mock<IMessageCoordinator>();
+
+            var expectedMessage = new Message("body", "subject");
+            var expectedRecipients = new List<MessageRecipient>();
+
+            var sut = new MessageService(mockAdminSvc.Object, mockMessageCoordinator.Object);
+            sut.SendMessageToDisasterVolunteers(expectedMessage, new RecipientCriterion(42), sender);
+
+            mockMessageCoordinator.Verify(x => x.SendMessage(expectedMessage, It.Is<List<MessageRecipient>>(y => y.Count.Equals(2)), sender), Times.Once());
+        }
     }
 }
--- crisischeckin/Services/RecipientCriterion.cs
+++ crisischeckin/Services/RecipientCriterion.cs
@@ -4,8 +4,6 @@ namespace Services
     public class RecipientCriterion
     {
         public int DisasterId { get; private set; }
-        public int? ClusterId { get; private set; }
-
         public IEnumerable<int> ClusterIds { get; private set; }
 
         public bool ClusterCoordinatorsOnly { get; private set; }
