705f3de51625ec54ae5ee5b92d55d044aaa7c028
Jonathas Sucupira <jonathas@jsucupira.com>
2015-09-30 22:51:33 -0400

    Fixing issue #334

--- crisischeckin/crisicheckinweb/scripts/App/accountRegister.js
+++ crisischeckin/crisicheckinweb/scripts/App/accountRegister.js
@@ -1,12 +1,5 @@
 ﻿(function ($) {
 
-    function validateEmail(email, regex) {
-        if (!regex) {
-            regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-        }
-        return regex.test(email);
-    }
-
     function checkUsernameExists(userName, parent) {
         if (!userName || userName.trim().length < 3) {
             if ($(parent).next().hasClass("feedbackHelper")) $(parent).next().remove();
@@ -76,8 +69,45 @@
         });
     };
 
+    function validateEmail(email, regex) {
+        if (!regex) {
+            regex = new RegExp(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i);
+        }
+        return regex.test(email);
+    }
+
     function requiredMessage(element, fieldType) {
         element.after('<div class=\"feedbackHelper failure\">' + fieldType + ' is required.</div>');
+    }
+
+    function clearWarning(element) {
+        if (element.next().hasClass("feedbackHelper"))
+            element.next().remove();
+    }
+
+    function addErrorMessage(element, message) {
+        element.after('<div class=\"feedbackHelper failure\">' + message + '</div>');
+    }
+
+    function validateRequiredWithMaxLength(element, fieldName) {
+        if (element.val().length === 0) {
+            requiredMessage(element, fieldName);
+        } else {
+            var lengthRequirement = element.attr("data-val-length-max");
+            if (lengthRequirement) {
+                try {
+                    lengthRequirement = parseInt(lengthRequirement);
+                    if (element.val().length > lengthRequirement) {
+                        var message = element.attr("data-val-length");
+                        if (!message)
+                            message = fieldName + " length is not valid.";
+                        addErrorMessage(element, message);
+                    }
+                } catch (e) {
+
+                }
+            }
+        }
     }
 
     $(document).ready(function () {
@@ -85,34 +115,76 @@
             delay: 300,
             event: 'keyup',
             fn: function () {
-                checkUsernameExists($("#txt_userName").val(), $("#txt_userName"));
+                if ($("#txt_userName").val().length > 0) {
+                    checkUsernameExists($("#txt_userName").val(), $("#txt_userName"));
+                }
             }
         });
+
         $("#txt_password").delay({
             delay: 300,
             event: 'keyup',
             fn: function () {
-                checkPasswordValidity($("#txt_userName").val(), $("#txt_password").val(), $("#txt_password"));
+                if ($("#txt_password").val().length > 0) {
+                    checkPasswordValidity($("#txt_userName").val(), $("#txt_password").val(), $("#txt_password"));
+                }
             }
         });
 
         $("#Email").focusout(function () {
-            var ele = $(this);
-            if (ele.next().hasClass("feedbackHelper"))
-                ele.next().remove();
+            var element = $(this);
+            clearWarning(element);
 
-            if (ele.val().length === 0) {
-                requiredMessage(ele, "Email Address");
+            if (element.val().length === 0) {
+                requiredMessage(element, "Email Address");
             } else {
-                var regex = ele.attr("data-val-regex-pattern");
+                var regex = element.attr("data-val-regex-pattern"); //follow pattern set in code
                 if (regex) {
                     regex = new RegExp(regex);
                 }
-
-                if (!validateEmail(ele.val(), regex)) {
-                    ele.after('<div class=\"feedbackHelper failure\">This is not a valid email address</div>');
+                if (!validateEmail(element.val(), regex)) {
+                    var message = element.attr("data-val-regex");
+                    if (!message)
+                        message = "This is not a valid email address.";
+                    addErrorMessage(element, message);
                 }
             }
         });
+
+        $("#FirstName").focusout(function () {
+            var element = $(this);
+            clearWarning(element);
+            validateRequiredWithMaxLength(element, "First Name");
+        });
+
+        $("#LastName").focusout(function () {
+            var element = $(this);
+            clearWarning(element);
+            validateRequiredWithMaxLength(element, "Last Name");
+        });
+
+        $("#PhoneNumber").focusout(function () {
+            var element = $(this);
+            clearWarning(element);
+            validateRequiredWithMaxLength(element, "Phone Number");
+        });
+
+        $("#txt_userName").focusout(function () {
+            var element = $(this);
+            clearWarning(element);
+            if (element.val().length === 0) {
+                requiredMessage(element, "User Name");
+            }
+        });
+
+        $("#txt_password").focusout(function () {
+            var element = $(this);
+            clearWarning(element);
+            if (element.val().length === 0) {
+                requiredMessage(element, "Password");
+            }
+        });
     });
+
+
 })(jQuery);
