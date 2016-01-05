611bf38d2c24ea796cfd8c6d17415b919dca10e7
Ryan <mcguirr@ccf.org>
2016-01-05 14:45:18 -0500

    Cleaned up a javascript workaround.

--- crisischeckin/crisicheckinweb/scripts/App/disasterEdit.js
+++ crisischeckin/crisicheckinweb/scripts/App/disasterEdit.js
@@ -4,16 +4,8 @@
         allChecked = allChecked && this.checked;
     });
     jQuery("#SelectedDisasterClustersAll").prop("indeterminate", !allChecked);
-    if (!allChecked) {
-        jQuery("#SelectedDisasterClustersAll").addClass("wasIndeterminate");
-    }
+    jQuery("#SelectedDisasterClustersAll").prop("checked", allChecked);
     jQuery("#SelectedDisasterClustersAll").click(function () {
-        // The first click of the Select All checkbox that started in the indeterminate state
-        // should make it checked, not unchecked (which is the default).
-        if (jQuery(this).hasClass("wasIndeterminate")) {
-            jQuery(this).prop("checked", true);
-            jQuery(this).removeClass("wasIndeterminate");
-        }
         if (this.checked) {
             jQuery("input[type=checkbox][id^=SelectedDisasterClusters]").prop("checked", true);
         }
