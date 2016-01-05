f3014eaa45c27f7a00f048305b5413c6594f687d
Ryan <mcguirr@ccf.org>
2016-01-05 14:15:33 -0500

    On the Create/Edit Disaster page, add a checkbox to allow the user to check or uncheck all cluster checkboxes.

---
+++ crisischeckin/crisicheckinweb/scripts/App/disasterEdit.js
@@ -0,0 +1,24 @@
+﻿jQuery(document).ready(function () {
+    var allChecked = true;
+    jQuery("input[type=checkbox][id^=SelectedDisasterClusters]").each(function () {
+        allChecked = allChecked && this.checked;
+    });
+    jQuery("#SelectedDisasterClustersAll").prop("indeterminate", !allChecked);
+    if (!allChecked) {
+        jQuery("#SelectedDisasterClustersAll").addClass("wasIndeterminate");
+    }
+    jQuery("#SelectedDisasterClustersAll").click(function () {
+        // The first click of the Select All checkbox that started in the indeterminate state
+        // should make it checked, not unchecked (which is the default).
+        if (jQuery(this).hasClass("wasIndeterminate")) {
+            jQuery(this).prop("checked", true);
+            jQuery(this).removeClass("wasIndeterminate");
+        }
+        if (this.checked) {
+            jQuery("input[type=checkbox][id^=SelectedDisasterClusters]").prop("checked", true);
+        }
+        else {
+            jQuery("input[type=checkbox][id^=SelectedDisasterClusters]").prop("checked", false);
+        }
+    });
+});
