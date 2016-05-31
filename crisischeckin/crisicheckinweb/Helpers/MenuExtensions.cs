0e361b77fe7d0897162416b27a07413afd5b8cb3
andrewhart098 <andrewthart098@gmail.com>
2016-05-31 14:29:04 -0400

    Issue #502 - Create separate page for Field reported data

--- crisischeckin/crisicheckinweb/Helpers/MenuExtensions.cs
+++ crisischeckin/crisicheckinweb/Helpers/MenuExtensions.cs
@@ -13,7 +13,7 @@ public static class MenuExtensions
         var routeData = htmlHelper.ViewContext.RouteData;
         var currentAction = routeData.GetRequiredString("action");
         var currentController = routeData.GetRequiredString("controller");
-        if (string.Equals(currentAction, action, StringComparison.OrdinalIgnoreCase) || string.Equals(currentController, controller, StringComparison.OrdinalIgnoreCase))
+        if (string.Equals(currentAction, action, StringComparison.OrdinalIgnoreCase) && string.Equals(currentController, controller, StringComparison.OrdinalIgnoreCase))
         {
             li.AddCssClass("active");
         }
---
+++ crisischeckin/crisicheckinweb/ViewModels/ResourceCheckinsListResultsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Models;
+
+namespace crisicheckinweb.ViewModels
+{
+    public class ResourceCheckinsListResultsViewModel
+    {
+        public ResourceCheckinsListResultsViewModel()
+        {
+            ResourceCheckins = new List<Resource>();
+        }
+
+        public List<Resource> ResourceCheckins { get; set; }
+    }
+}
