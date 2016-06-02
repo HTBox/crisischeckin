096b09c45242c71483cf3beb889272970f5bfc9e
andrewhart098 <andrewthart098@gmail.com>
2016-06-02 17:41:39 -0400

    Initial design of Requests Index and Create page done

--- crisischeckin/Models/Request.cs
+++ crisischeckin/Models/Request.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,14 @@ namespace Models
     public class Request
     {
         public int RequestId { get; set; }
+        [DisplayName("Created On")]
         public DateTime CreatedDate { get; set; }
+        [DisplayName("End Date")]
         public DateTime EndDate { get; set; }
         public string Description { get; set; }
         public int OrganizationId { get; set; }
         public int CreatorId { get; set; }
+        [DisplayName("Status")]
         public bool Completed { get; set; }
         public string Location { get; set; }
         public List<RequestAssigniee> Assigniees { get; set; } 
