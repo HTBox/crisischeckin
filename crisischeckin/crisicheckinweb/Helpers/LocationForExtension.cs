a864c5cfb49673ffa732d3e6d14de3d22e1dbd25
andrewhart098 <andrewthart098@gmail.com>
2016-06-13 15:21:30 -0400

    Basic CRUD for Resources done

---
+++ crisischeckin/crisicheckinweb/Helpers/LocationForExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models;
+using RestSharp.Extensions;
+
+namespace crisicheckinweb.Helpers
+{
+    public static class LocationForExtension
+    {
+        public static MvcHtmlString LocationFor(this HtmlHelper htmlHelper, Address address)
+        {
+            string adr = address.BuildingName + "<br/>"
+                         + (address.AddressLine1.HasValue() ? (address.AddressLine1 + "<br/>") : "")
+                         + (address.AddressLine2.HasValue() ? (address.AddressLine2 + "<br/>") : "")
+                         + (address.AddressLine3.HasValue() ? (address.AddressLine3 + "<br/>") : "")
+                         + (address.City.HasValue() ? (address.City) : "")
+                         + (address.County.HasValue() ? (" (" + address.County + ")") : "")
+                         + ((!address.City.HasValue() && !address.County.HasValue()) ? "" : ", ")
+                         + (address.State.HasValue() ? (address.State + " ") : "")
+                         + (address.PostalCode.HasValue() ? (address.PostalCode + " ") : "");
+            return MvcHtmlString.Create(adr);
+        }
+    }
+}
