44e436869defcd10a3c5d078f5a0da36d7573588
andrewhart098 <andrewthart098@gmail.com>
2016-06-14 22:56:56 -0400

    added search models

--- crisischeckin/crisicheckinweb/Controllers/ResourcesController.cs
+++ crisischeckin/crisicheckinweb/Controllers/ResourcesController.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using crisicheckinweb.ViewModels;
+using crisicheckinweb.ViewModels.SearchModels;
 using crisicheckinweb.Wrappers;
 using Models;
 using WebMatrix.WebData;
@@ -24,10 +26,20 @@ namespace crisicheckinweb.Controllers
         }
 
         // GET: Resources
-        public async Task<ActionResult> Index()
+        public async Task<ActionResult> Index(IQueryable<Resource> filteredResources = null)
         {
-            var resources = db.Resources.Include(r => r.Disaster).Include(r => r.Person).Include(r => r.ResourceType);
-            return View(await resources.ToListAsync());
+            IQueryable<Resource> resources;
+
+            resources = filteredResources ??
+                        db.Resources.Include(r => r.Disaster)
+                                    .Include(r => r.Person)
+                                    .Include(r => r.ResourceType);
+
+            return View(new AdminResourceIndexViewModel()
+            {
+                Resources =  await resources.ToListAsync(),
+                ResourceSearch = new ResourceSearch(await db.Disasters.ToListAsync(), await db.ResourceTypes.ToListAsync())
+            });
         }
 
         // GET: Resources/Details/5
@@ -146,6 +158,99 @@ namespace crisicheckinweb.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        [HttpPost, ActionName("Filter")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Filter(ResourceSearch specifiedResource)
+        {
+            var resource = db.Resources.Include(r => r.Disaster)
+                                    .Include(r => r.Person)
+                                    .Include(r => r.ResourceType);
+
+            if (specifiedResource.Description != null)
+            {
+                resource = resource.Where(x => x.Description.Contains(specifiedResource.Description));
+            }
+
+            if (specifiedResource.NullableCreatedDate != null)
+            {
+                resource = resource.Where(x => DbFunctions.TruncateTime(x.EntryMade) == DbFunctions.TruncateTime(specifiedResource.NullableCreatedDate));
+            }
+
+            if (specifiedResource.NullableStartDate != null)
+            {
+                resource = resource.Where(x => DbFunctions.TruncateTime(x.StartOfAvailability) == DbFunctions.TruncateTime(specifiedResource.NullableStartDate));
+            }
+
+            if (specifiedResource.NullableEndDate != null)
+            {
+                resource = resource.Where(x => DbFunctions.TruncateTime(x.EndOfAvailability) == DbFunctions.TruncateTime(specifiedResource.NullableEndDate));
+            }
+
+            if (specifiedResource.Location.AddressLine1 != null)
+            {
+                resource = resource.Where(x => x.Location.AddressLine1.Contains(specifiedResource.Location.AddressLine1));
+            }
+
+            if (specifiedResource.Location.AddressLine2 != null)
+            {
+                resource = resource.Where(x => x.Location.AddressLine2.Contains(specifiedResource.Location.AddressLine2));
+            }
+
+            if (specifiedResource.Location.AddressLine3 != null)
+            {
+                resource = resource.Where(x => x.Location.AddressLine3.Contains(specifiedResource.Location.AddressLine3));
+            }
+
+            if (specifiedResource.Location.City != null)
+            {
+                resource = resource.Where(x => x.Location.City.Contains(specifiedResource.Location.City));
+            }
+
+            if (specifiedResource.Location.County != null)
+            {
+                resource = resource.Where(x => x.Location.County.Contains(specifiedResource.Location.County));
+            }
+
+            if (specifiedResource.Location.State != null)
+            {
+                resource = resource.Where(x => x.Location.State.Contains(specifiedResource.Location.State));
+            }
+
+            if (specifiedResource.Location.Country != null)
+            {
+                resource = resource.Where(x => x.Location.Country.Contains(specifiedResource.Location.Country));
+            }
+
+            if (specifiedResource.Location.PostalCode != null)
+            {
+                resource = resource.Where(x => x.Location.PostalCode.Contains(specifiedResource.Location.PostalCode));
+            }
+
+            if (specifiedResource.Qty > 0.1m)
+            {
+                resource = resource.Where(x => x.Qty == specifiedResource.Qty);
+            }
+
+            if (specifiedResource.SelectedResourceTypeId != null)
+            {
+                resource = resource.Where(x => x.ResourceTypeId == specifiedResource.SelectedResourceTypeId);
+            }
+
+            if (specifiedResource.SelectedDisasterId != null)
+            {
+                resource = resource.Where(x => x.DisasterId == specifiedResource.SelectedDisasterId);
+            }
+
+            if (specifiedResource.Status != null)
+            {
+                resource = resource.Where(x => x.Status == specifiedResource.Status);
+            }
+
+            // Left Off - 
+            return RedirectToAction("Index", resource);
         }
     }
 }
