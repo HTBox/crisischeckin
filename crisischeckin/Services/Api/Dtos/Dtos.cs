4b79f039cc496083650789e80fe3402ba569e9bb
Hong <h@hzhang.net>
2016-06-18 12:13:42 -0400

    Added  GetRequests api endpoint and its associated model RequestDto (#666)

--- crisischeckin/Services/Api/Dtos/Dtos.cs
+++ crisischeckin/Services/Api/Dtos/Dtos.cs
@@ -29,5 +29,24 @@ namespace Services.Api.Dtos
 
         public DisasterDto Disaster { get; set; }
     }
+
+    public class RequestDto
+    {
+        public int RequestId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+        public int CreatorId { get; set; }
+        public int? AssigneeId { get; set; }
+        public int? OrganizationId { get; set; }
+
+        public bool Completed { get; set; }
+        public string Location { get; set; }
+
+        //TODO:
+        //public virtual Person Creator { get; set; }
+        //public virtual Person Assignee { get; set; }
+        //public virtual Organization Organization { get; set; }
+    }
     #endregion
 }
