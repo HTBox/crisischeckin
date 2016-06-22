94bb20b687d84b3aab2d0642e50d96e5e883bf9f
andrewhart098 <andrewthart098@gmail.com>
2016-06-22 10:37:50 -0400

    Added tabbed menu

---
+++ crisischeckin/CrisisCheckinMobile/CrisisCheckinMobile/Views/MainTabbedView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CrisisCheckinMobile.Views
+{
+    public partial class MainTabbedView : TabbedPage
+    {
+        public MainTabbedView()
+        {
+            BackgroundColor = Color.White;
+
+            var homePage = new NavigationPage(new ProfileView());
+            homePage.Title = "Home";
+            homePage.Icon = "iosMenuIcons/CCI-menuHomeHi.png";
+            homePage.BarBackgroundColor = Constants.HtBoxRed;
+            homePage.BarTextColor = Color.White;
+            Children.Add(homePage);
+
+            Children.Add(new ResourceListPage()
+            {
+                Title = "Resources",
+                Icon = "iosMenuIcons/CCI-menuSettingHi.png"
+            });
+
+            Children.Add(new MyRequests()
+            {
+                Title = "Requests",
+                Icon = "iosMenuIcons/CCI-menuListHi.png"
+            });
+
+            Children.Add(new TemporaryView()
+            {
+                Title = "Report",
+                Icon = "iosMenuIcons/CCI-menuRptHi.png"
+            });
+
+        }
+    }
+}
