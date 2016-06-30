2a93806f2c2b2bf2e28c7b03f9bcb5c441756dee
andrewhart098 <andrewthart098@gmail.com>
2016-06-30 12:04:32 -0400

    Request List and Detail pages now reflect app style.  Request Xaml page removed

---
+++ crisischeckin/CrisisCheckinMobile/CrisisCheckinMobile/Views/RequestDetailPage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrisisCheckinMobile.Models;
+using Xamarin.Forms;
+
+namespace CrisisCheckinMobile.Views
+{
+    class RequestDetailPage : ContentPage
+    {
+
+        public RequestDetailPage(RequestDto request)
+        {
+            var task = Init(request);
+        }
+
+        private async Task Init(RequestDto selectedRequest)
+        {
+            BackgroundColor = Constants.HtBoxDarkBrown;
+            Title = selectedRequest.Description;
+
+            var descriptionLabel = new Label
+            {
+                Text = selectedRequest.Description,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                TextColor = Constants.HtBoxLightBrown
+            };
+
+            var locationLabel = new Label
+            {
+                Text = $"Location: {selectedRequest.Location}",
+                FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                TextColor = Color.White
+            };
+
+            var createdLabel = new Label
+            {
+                Text = $"Created On: {selectedRequest.CreatedDate.ToString("MMMM dd, yyyy H:mm")}",
+                FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                TextColor = Color.White
+            };
+
+            var endLabel = new Label
+            {
+                Text = $"Ends On: {selectedRequest.EndDate.ToString("MMMM dd, yyyy H:mm")}",
+                FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                TextColor = Color.White
+            };
+
+            Content = new ScrollView
+            {
+                Content = new StackLayout
+                {
+                    Spacing = 10,
+                    Padding =  10,
+                    Children = { descriptionLabel, locationLabel, createdLabel, endLabel }
+                }
+            };
+        }
+    }
+}
---
+++ crisischeckin/CrisisCheckinMobile/CrisisCheckinMobile/Views/RequestListPage.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrisisCheckinMobile.ApiClient;
+using CrisisCheckinMobile.Models;
+using Xamarin.Forms;
+
+namespace CrisisCheckinMobile.Views
+{
+    public class RequestListPage : ContentPage
+    {
+        private ListView _requestListView;
+        public IEnumerable<ItemsView> ListViewRequests { get; set; }
+        private EventHandler<SelectedItemChangedEventArgs> _itemSelectedEventHandler;
+
+        public RequestListPage()
+        {
+            var task = Init();
+        }
+
+        private async Task Init()
+        {
+            _itemSelectedEventHandler = (sender, args) =>
+            {
+                if (args.SelectedItem == null)
+                    return;
+
+                var selectedItem = args.SelectedItem as RequestDto;
+
+                Navigation.PushAsync(new RequestDetailPage(selectedItem));
+            };
+
+            ICrisisCheckInApiClient apiClient = new CrisisCheckInApiClient();
+            var requests = await apiClient.GetRequests(2);
+
+            var cell = CreateRequestCell();
+
+            Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+            Title = "Requests";
+            BackgroundColor = Constants.HtBoxDarkBrown;
+
+            _requestListView = new ListView()
+            {
+                ItemsSource = requests,
+                ItemTemplate = cell,
+                BackgroundColor = Constants.HtBoxDarkBrown
+            };
+            _requestListView.ItemSelected += _itemSelectedEventHandler;
+            Content = _requestListView;
+        }
+
+        private DataTemplate CreateRequestCell()
+        {
+            var cell = new DataTemplate(typeof (TextCell));
+            cell.SetBinding(TextCell.TextProperty, new Binding("Description"));
+            cell.SetBinding(TextCell.DetailProperty, new Binding("Location"));
+            cell.SetValue(TextCell.TextColorProperty, Color.White);
+            cell.SetValue(TextCell.DetailColorProperty, Constants.HtBoxLightBrown);
+            return cell;
+        }
+    }
+}
